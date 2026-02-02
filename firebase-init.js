20639ac4477d97c402531f29882ce43b55b36c7a
IFWLOWNER <ben.leahy@ifwl.net>
2026-02-02 20:43:14 +0000

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,1 +1,20 @@
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyDw7lK1obLfMziXFr7gJr5R5huFGjfVcc8",
+    authDomain: "ifwl-591d8.firebaseapp.com",
+    projectId: "ifwl-591d8",
+    storageBucket: "ifwl-591d8.firebasestorage.app",
+    messagingSenderId: "703021152109",
+    appId: "1:703021152109:web:18e2f6a73e0521e4850c64",
+    measurementId: "G-974R4YE6L5"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  const db = firebase.firestore();
+</script>
 
