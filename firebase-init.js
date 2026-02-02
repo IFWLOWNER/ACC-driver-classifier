c703826760b8fe694458f2f8cc02785d994c103e
IFWLOWNER <ben.leahy@ifwl.net>
2026-02-02 20:51:34 +0000

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,20 +1,21 @@
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js"></script>
+// firebase-init.js
+// Firebase compat SDK (keeps the code simple for static pages)
+import "https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js";
+import "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth-compat.js";
+import "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js";
 
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyDw7lK1obLfMziXFr7gJr5R5huFGjfVcc8",
-    authDomain: "ifwl-591d8.firebaseapp.com",
-    projectId: "ifwl-591d8",
-    storageBucket: "ifwl-591d8.firebasestorage.app",
-    messagingSenderId: "703021152109",
-    appId: "1:703021152109:web:18e2f6a73e0521e4850c64",
-    measurementId: "G-974R4YE6L5"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDw7lK1obLfMziXFr7gJr5R5huFGjfVcc8",
+  authDomain: "ifwl-591d8.firebaseapp.com",
+  projectId: "ifwl-591d8",
+  storageBucket: "ifwl-591d8.firebasestorage.app",
+  messagingSenderId: "703021152109",
+  appId: "1:703021152109:web:18e2f6a73e0521e4850c64",
+  measurementId: "G-974R4YE6L5"
+};
 
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-</script>
+firebase.initializeApp(firebaseConfig);
 
+export const auth = firebase.auth();
+export const db = firebase.firestore();
+export const firebaseNS = firebase;
